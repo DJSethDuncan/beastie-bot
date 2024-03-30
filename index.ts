699b17d684c2c1d0ca43db9c03892b38d2a0981b
Seth Duncan <sethduncan@gmail.com>
2024-03-29 23:50:39 -0500

    add sentry

--- index.ts
+++ index.ts
@@ -2,7 +2,18 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import * as Discord from "discord.js";
 const client = new Discord.Client();
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import { processMessage } from "./bin/processMessage";
+
+Sentry.init({
+  dsn: "https://cf0fc945bcac2d5808c37c2181fa3ce5@o4506998133620736.ingest.us.sentry.io/4506998137290752",
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 
 client.once("ready", (): void => {
   console.log("BeastieBot is about to drop");
